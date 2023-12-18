e6737933b3f9ca2708c20d784ef4ec5563a3aa36
Matthias-Thiele <matthias@mmth.de>
2023-12-18 14:37:13 +0100

    Check git tracking

--- src/java/de/mmth/db/RoleTable.java
+++ src/java/de/mmth/db/RoleTable.java
@@ -4,10 +4,26 @@
  */
 package de.mmth.db;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  *
  * @author matthias
  */
-public class RoleTable {
+public class RoleTable extends DBTable {
+  private static final Logger logger = LogManager.getLogger(RoleTable.class);
+  
+  private static final String TABLE_CONFIG = 
+    """
+    id I G
+    name V 100
+    supervisor I
+    """;
+  
+  public RoleTable(DBConnect conn, String tableName) {
+    super(conn, tableName, TABLE_CONFIG);
+  }
+  
   
 }
